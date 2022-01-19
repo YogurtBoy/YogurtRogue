72060623c3ee93e5da4e75e5cacd4267fd73a99a
Joe Parnell <jparnell@echodyne.com>
2022-01-19 14:25:17 -0800

    Part 3 done.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tcod 
 
 from engine import Engine
-from game_map import GameMap
+from procgen import generate_dungeon
 from entity import Entity
 from input_handlers import EventHandler
 
@@ -15,6 +15,10 @@ def main() -> None:
     map_width = SCREEN_WIDTH
     map_height = SCREEN_HEIGHT - 5
 
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
+
     tileset = tcod.tileset.load_tilesheet("dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD)
 
     event_handler = EventHandler()
@@ -23,7 +27,14 @@ def main() -> None:
     npc = Entity(int(SCREEN_WIDTH/2 - 5), int(SCREEN_HEIGHT/2 - 5), "@", (255, 255, 0))
     entities = {npc, player}
 
-    game_map = GameMap(width=map_width, height=map_height)
+    game_map = generate_dungeon(
+        max_rooms=max_rooms, 
+        room_min_size=room_min_size,
+        room_max_size=room_max_size, 
+        map_width=map_width, 
+        map_height=map_height, 
+        player=player, 
+    )
 
     sterling = Engine(entities=entities, 
                       event_handler=event_handler, 
