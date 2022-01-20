42aa718daeab824593483f66307b3a271e28d3e5
Joe Parnell <jparnell@echodyne.com>
2022-01-20 10:27:12 -0800

    Part 5 done.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+""" http://rogueliketutorials.com/tutorials/tcod/v2/ """
+
+import copy
+
 import tcod 
 
 from engine import Engine
 from procgen import generate_dungeon
-from entity import Entity
+import entity_factories
 from input_handlers import EventHandler
-
-""" http://rogueliketutorials.com/tutorials/tcod/v2/ """
 
 
 def main() -> None:
@@ -19,13 +21,13 @@ def main() -> None:
     room_min_size = 6
     max_rooms = 30
 
+    max_monsters_per_room = 2
+
     tileset = tcod.tileset.load_tilesheet("dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD)
 
     event_handler = EventHandler()
 
-    player = Entity(int(SCREEN_WIDTH/2), int(SCREEN_HEIGHT/2), "@", (255, 255, 255))
-    npc = Entity(int(SCREEN_WIDTH/2 - 5), int(SCREEN_HEIGHT/2 - 5), "@", (255, 255, 0))
-    entities = {npc, player}
+    player = copy.deepcopy(entity_factories.player)
 
     game_map = generate_dungeon(
         max_rooms=max_rooms, 
@@ -33,11 +35,10 @@ def main() -> None:
         room_max_size=room_max_size, 
         map_width=map_width, 
         map_height=map_height, 
-        player=player, 
-    )
+        max_monsters_per_room=max_monsters_per_room,
+        player=player)
 
-    sterling = Engine(entities=entities, 
-                      event_handler=event_handler, 
+    sterling = Engine(event_handler=event_handler, 
                       game_map=game_map, 
                       player=player)
 
